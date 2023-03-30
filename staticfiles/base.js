71ab127c17b71f0bbbf4d50e363ed46a44254a18
Christian Storgaard <chris.storgaard@gmail.com>
2023-03-30 18:11:04 -0400

    please

--- staticfiles/base.js
+++ staticfiles/base.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://trivioliv.herokuapp.com/api/questions/'; // replace with your API endpoint URL
+const apiUrl = 'http://trivioliv.herokuapp.com/api/questions/?'; // replace with your API endpoint URL
 
 // make a fetch request to the API
 fetch(apiUrl)
