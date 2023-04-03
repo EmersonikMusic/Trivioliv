71c6da3269a17a34e61f2d583d383ee50a0b4260
Christian Storgaard <chris.storgaard@gmail.com>
2023-04-03 13:13:39 -0400

    more cors23094

--- Triviolivia/settings.py
+++ Triviolivia/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*', 'https://markadegames.com', 'https://www.markadegames.com'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://markadegames.com",
+    "markadegames.com/:1S",
 ]
 
 # Application definition
