b47eb723ce218f408bee57178d3c7213ec7c8b14
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-01-05 19:31:47 -0500

    nope I broke it - mabye this?

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -471,7 +471,7 @@ function enable_era(clicked_id) {
 
   function allNoneDifficultiesButton() {
       if (all_none_difficulties == true) {
-        for (var i = 1; i < difficultyButtons.length + 1; i++) {
+        for (var i = 0; i < difficultyButtons.length; i++) {
             difficultyButtons[String(i)].classList.remove('active');
             difficultyButtons[String(i)].classList.add('inactive');
             disable_difficulty(String(i));
@@ -479,7 +479,7 @@ function enable_era(clicked_id) {
         }
         all_none_difficulties = false;
         } else {
-        for (var i = 1; i < difficultyButtons.length + 1; i++) {
+        for (var i = 0; i < difficultyButtons.length; i++) {
             difficultyButtons[String(i)].classList.remove('inactive');
             difficultyButtons[String(i)].classList.add('active');
             enable_difficulty(String(i));
@@ -490,7 +490,7 @@ function enable_era(clicked_id) {
   
   function allNoneErasButton() {
       if (all_none_eras == true) {
-        for (var i = 1; i < eraButtons.length + 1; i++) {
+        for (var i = 0; i < eraButtons.length; i++) {
             eraButtons[String(i)].classList.remove('active');
             eraButtons[String(i)].classList.add('inactive');
             document.getElementById("demo").innerHTML = 'You must select at least one era before starting the game.';
