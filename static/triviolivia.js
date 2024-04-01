3f7f9f11b3aed231f7bd1640a41938964000810d
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-04-01 16:54:00 -0400

    try again

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -615,7 +615,7 @@ function changeButtonText() {
 
   progressBar.addEventListener("animationiteration", function() {
     if (!isPaused) {
-      progressBar.style.animation = "replenishProgress " + time_per_answer + "s linear reverse, shrinkProgress " + time_per_question + "s linear reverse";
+        progressBar.style.animation = "replenishProgress " + time_per_answer + "s linear, shrinkProgress " + time_per_question + "s linear forwards";
     }
   });
 
