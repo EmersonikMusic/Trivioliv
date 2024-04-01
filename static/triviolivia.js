2b600dd629498c115cf7636c86bb428d27c2b153
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-04-01 16:56:08 -0400

    undo

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -615,7 +615,7 @@ function changeButtonText() {
 
   progressBar.addEventListener("animationiteration", function() {
     if (!isPaused) {
-        progressBar.style.animation = "replenishProgress " + time_per_answer + "s linear, shrinkProgress " + time_per_question + "s linear forwards";
+        progressBar.style.animation = "replenishProgress " + time_per_answer + "s linear forwards, shrinkProgress " + time_per_question + "s linear forwards";
     }
   });
 
