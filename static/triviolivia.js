009c0db26bd2eceb21c84c7663d49714beff17c1
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-04-01 15:45:16 -0400

    yet another

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -477,18 +477,18 @@ function enable_era(clicked_id) {
 //Functions for ALL/NONE buttons
 function allNoneCategoriesButton() {
     if (all_none_categories == true) {
-        for (var i = 1; i < categoryButtons.length; i++) {
-            categoryButtons[String(i)].classList.remove('active');
-            categoryButtons[String(i)].classList.add('inactive');
+        for (var i = 0; i < categoryButtons.length; i++) {
+            categoryButtons[String(i+1)].classList.remove('active');
+            categoryButtons[String(i+1)].classList.add('inactive');
             disable_category(String(i));
             document.getElementById("demo").innerHTML = 'You must select at least one category before starting the game.';
         }
         all_none_categories = false;
     } else {
         category_list = [];
-        for (var i = 1; i < categoryButtons.length; i++) {
-            categoryButtons[String(i)].classList.remove('inactive');
-            categoryButtons[String(i)].classList.add('active');
+        for (var i = 0; i < categoryButtons.length; i++) {
+            categoryButtons[String(i+1)].classList.remove('inactive');
+            categoryButtons[String(i+1)].classList.add('active');
             enable_category(String(i));
         }
         all_none_categories = true;
