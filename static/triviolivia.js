5d964f06846ee03b136f4ee9c3e84ecc821fceb5
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-04-01 20:59:28 -0400

    unreal   set qa time to 1s to shred through 50q game for test

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -4,8 +4,8 @@ const answerDisplay = document.querySelector('.answer-container')
 
 // Declaring game setting variables
 var number_of_questions = 2;
-var time_per_question = 10;
-var time_per_answer = 10;
+var time_per_question = 1;
+var time_per_answer = 1;
 var game_started = false;
 var game_paused = false;
 var menu_hidden = false;
