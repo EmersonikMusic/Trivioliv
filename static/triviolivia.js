493173a0d3425f0784411f9399b556c6f6a6e32c
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-04-17 17:37:12 -0400

    undo hard

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -316,7 +316,7 @@ const showQuestion = (displayed_question) => {
     div.style.opacity = 1;
     const messageElement = document.createElement('p')
     messageElement.textContent = displayed_question
-    questionDisplay.html(messageElement)
+    questionDisplay.append(messageElement)
     setTimeout(() => questionDisplay.removeChild(messageElement), time_per_question * 1000 + time_per_answer * 1000)
 }
 
@@ -325,7 +325,7 @@ const showAnswer = (displayed_answer) => {
     div.style.opacity = 0.7;
     const messageElement = document.createElement('p')
     messageElement.textContent = displayed_answer
-    answerDisplay.html(messageElement)
+    answerDisplay.append(messageElement)
     setTimeout(() => answerDisplay.removeChild(messageElement), time_per_answer * 1000)
 }
 
