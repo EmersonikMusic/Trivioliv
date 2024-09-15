c715383b2baf40a632687cfbc856b764c1375956
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-09-14 21:20:24 -0400

    undo everything

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -363,8 +363,7 @@ const mainGameFunction = async () => {
         }
 
         document.body.style.background = category_colors[globalData[i].category_name];
-        document.body.style.character = TO_OG_SVG.svg;
-
+        
         let questionTimeRemaining = time_per_question * 10; // Convert to tenths of a second
         let answerTimeRemaining = time_per_answer * 10; // Convert to tenths of a second
         
