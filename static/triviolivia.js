6ec9f9653916c0a07e242587d4f73b84cca14eab
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-09-14 21:45:28 -0400

    character wipe upon category change attempt

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -363,6 +363,7 @@ const mainGameFunction = async () => {
         }
 
         document.body.style.background = category_colors[globalData[i].category_name];
+        document.character = none;
         
         let questionTimeRemaining = time_per_question * 10; // Convert to tenths of a second
         let answerTimeRemaining = time_per_answer * 10; // Convert to tenths of a second
