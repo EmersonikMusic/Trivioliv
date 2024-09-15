09bacdb1f5094fb5b9433ed318b2033f64cf9bbc
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2024-09-14 22:08:19 -0400

    answer slider bar fix attempt

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -630,4 +630,5 @@ startButton.addEventListener("click", function() {
   const progressElement = document.getElementById('progress');
   
   progressElement.style.animationDuration = `${time_per_question}s`;
+  progressElement.style.animationDuration = `${time_per_answer}s`;
   
