4c94f513ef6cc472420517d7f242c5a9972339af
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2025-02-08 18:01:41 -0500

    fullscreen mode attempt

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -7329,3 +7329,22 @@ function closeAboutUs() {
     card.style.display = 'none';
     overlay.style.display = 'none';
 }
+
+
+
+
+// Fullscreen mode attempt
+document.getElementById("fullscreenBtn").addEventListener("click", function() {
+  let elem = document.documentElement; // Fullscreen for the entire page
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { // Firefox
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, Opera
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE/Edge
+    elem.msRequestFullscreen();
+  }
+});
