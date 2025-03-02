262d05fb156254dc1c992b70a4f2b59309867904
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2025-03-01 22:11:07 -0500

    corrected typo

--- static/triviolivia.js
+++ static/triviolivia.js
@@ -2,7 +2,7 @@
 const contentDict = {
 
 
-  "moves": `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 310 491">
+  "movies": `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 310 491">
   <defs>
     <style>
       .st0 {
