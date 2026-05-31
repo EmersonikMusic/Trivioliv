c90fa300e39f42f97be8af070d89f9705b4bc9c6
EmersonikMusic <45269561+EmersonikMusic@users.noreply.github.com>
2026-05-30 21:25:26 -0400

    undo fully of views.py

--- api/views.py
+++ api/views.py
@@ -15,39 +15,45 @@ class QuestionList(generics.ListAPIView):
     serializer_class = QuestionSerializer
 
     def get_queryset(self):
-        """
-        Optionally restricts the returned purchases to a given user,
-        by filtering against a `username` query parameter in the URL.
-        """
-        queryset = Question.objects.all()
-        category = self.request.query_params.get('category')
-        era = self.request.query_params.get('era')
-        difficulty = self.request.query_params.get('difficulty')
-        questions = self.request.query_params.get('questions')
+            """
+            Optionally restricts the returned purchases to a given user,
+            by filtering against a `username` query parameter in the URL.
+            """
+            queryset = Question.objects.all()
+            category = self.request.query_params.get('category')
+            era= self.request.query_params.get('era')
+            difficulty = self.request.query_params.get('difficulty')
+            questions = self.request.query_params.get('questions')
 
-        if category is not None:
-            cat_list = category.split(',')
-            queryset = queryset.filter(category__id__in=cat_list)
-        
-        if era is not None:
-            era_list = era.split(',')
-            queryset = queryset.filter(eras__id__in=era_list)
-        
-        if difficulty is not None:
-            diff_list = difficulty.split(',')
-            queryset = queryset.filter(difficulty__id__in=diff_list)
-
-        num_questions = 1
-        if questions is not None:
-            num_questions = int(questions)
-
-        valid_ids = list(queryset.values_list('id', flat=True))
-        
-        if num_questions > len(valid_ids):
-            num_questions = len(valid_ids)
+            if category is not None:
+                cat_list = category.split(',')
+                queryset = queryset.exclude(category__id__in=cat_list)
             
-        random_ids = random.sample(valid_ids, num_questions)
-        return queryset.filter(id__in=random_ids).distinct()
+            if era is not None:
+                banned_era_list = era.split(',')
+                valid_eras = Era.objects.exclude(id__in=banned_era_list)
+                queryset = queryset.filter(eras__id__in=valid_eras)
+            
+            if difficulty is not None:
+                diff_list = difficulty.split(',')
+                queryset = queryset.exclude(difficulty__id__in=diff_list)
+
+            num_questions=1
+            if questions is not None:
+                num_questions=int(questions)
+
+            i=1
+            question_list=[]
+            q_list = queryset
+            print(num_questions)
+            while i <= num_questions:
+                random_question = random.choice(q_list)
+                question_list.append(random_question.id)
+                q_list = q_list.exclude(id=random_question.id)
+                i=i+1
+            print(question_list)
+
+            return queryset.filter(id__in=question_list).distinct()
     
 class QuestionDetail(APIView):
     def get_object(self, pk):
